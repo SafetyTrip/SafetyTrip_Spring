57e8560ad2e831167ea29b3b9cd46d07d5c782c7
joon <leejj90@naver.com>
2018-04-09 00:29:16 +0900

    city 수정

--- safety/src/main/webapp/resources/js/city.js
+++ safety/src/main/webapp/resources/js/city.js
@@ -9,7 +9,7 @@ $(document).ready( () => {
 	
 	$('.safety-tr').on('click', (e) => {
 		const couno = $('.page-header').attr('data-couno');
-		const sno = $(e.target).attr('data-sno');
+		const sno = $(e.target).closest('tr').attr('data-sno');
 		
 		location.href = '/trip/safety/'+couno+'?curPage=1&sno='+sno;
 	});
